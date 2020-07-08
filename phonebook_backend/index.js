d2459875356ec04a32004b8e285deba35b8ed4aa
Simon Nyman <nyman.simon@hotmail.com>
2020-07-08 20:55:21 +0300

    Exercise 3.2 done

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var moment = require('moment') // For easier time/date management
 const app = express()
 
 app.use(express.json())
@@ -12,7 +13,17 @@ let people = [
 
 // GET all numbers
 app.get('/api/persons', (request, response) => {
+  console.log("HEAD:",new Date().toISOString())
   response.json(people)
+})
+
+app.get('/info', (request, response) => {
+
+  let info = `<b>Phonebook has info for ${people.length} people</b>
+              </br></br>
+              ${moment().format('ddd MMM D YYYY HH:mm:ss [GMT]ZZ [(Eastern European Standard Time)]')}`
+
+  response.send(info)
 })
 
 const PORT = 3001
