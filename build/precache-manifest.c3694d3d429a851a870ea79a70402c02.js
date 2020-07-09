13bf41351de3575ac772527edae0e4f109d69288
Simon Nyman <nyman.simon@hotmail.com>
2020-07-09 18:25:07 +0300

    Exercise 3.11 done. Added production front end

---
+++ build/precache-manifest.c3694d3d429a851a870ea79a70402c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f0680513b5d6c718489cec8dd44264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19bec21eed9ce03c0465",
+    "url": "/static/css/main.2d6e9c40.chunk.css"
+  },
+  {
+    "revision": "c7a03807568ad8f550fa",
+    "url": "/static/js/2.bae63cfe.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/static/js/2.bae63cfe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19bec21eed9ce03c0465",
+    "url": "/static/js/main.bdbb71a3.chunk.js"
+  },
+  {
+    "revision": "4230d87a2d3cbabbdf4d",
+    "url": "/static/js/runtime-main.e53b3d9b.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 const morganConf = morgan((tokens, req, res) => {
   let baseConf = [
